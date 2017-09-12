0d7f0820d052bf19539591c86b6aa7007e62a8d2
Sasha Bajzek <dabridginator@gmail.com>
2017-09-12 12:25:36 -0700

    Add graph descriptions

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { loadAllData } from './DataHandling';
 
 import CountyMap from './components/CountyMap';
 import Histogram from './components/Histogram';
-import { Title, Description } from './components/Meta';
+import { Title, Description, GraphDescription } from './components/Meta';
 
 class App extends Component {
 	state = {
@@ -65,6 +65,8 @@ class App extends Component {
 				<Description data={filteredSalaries}
 										allData={this.state.techSalaries}
 										medianIncomesByCounty={this.state.medianIncomesByCounty}
+										filteredBy={this.state.filteredBy} />
+				<GraphDescription data={filteredSalaries}
 										filteredBy={this.state.filteredBy} />
 				<svg width="1100" height="500">
 					<CountyMap usTopoJson={this.state.usTopoJson}
